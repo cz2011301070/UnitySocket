065db55847ac8d2d176e0fe5084598f828cbe937
cz2011301070 <1908201909@qq.com>
2019-03-25 23:28:37 +0800

    Initial commit

---
+++ Commander/Assets/Command.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System;
+using UnityEngine.UI;
+using System.Threading;
+
+public class Command : MonoBehaviour {
+	public Camera Cam;
+
+
+	private IPAddress ip;
+	private EndPoint ep;
+
+	private Thread SendThread;
+	private Thread RecvThread;
+
+	private byte[] SendBuffer = new byte[100];
+	private byte[] RecvBuffer = new byte[1024];
+
+	//Create Socket
+	private  Socket client = new Socket (AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+
+	#region Start
+	void Start () {
+		//Start a thread for sending message
+		//SendThread = new Thread (SendMessageToServer);
+
+		//Prepare the IP+Port for connecting
+		ip=IPAddress.Parse("192.168.1.4");
+		ep = new IPEndPoint (ip, 6000);
+
+		//Connect to the server
+		client.Connect(ep);
+
+		//Send message to server
+		string message = "This is Client!";
+		SendBuffer = Encoding.UTF8.GetBytes (message);			
+		client.Send(SendBuffer);
+
+
+		//Start a thread for Receiving message
+		RecvThread = new Thread (RecvMessageFromServer);
+		RecvThread.IsBackground =true;
+		RecvThread.Start ();
+
+	}
+	#endregion
+
+	#region Update
+	void Update(){
+		//shutdown, close socket, close thread
+		//RecvMessageFromServer()
+		if (RecvBuffer != null) {
+			char ID = BitConverter.ToChar (RecvBuffer, 0);
+			if (ID == 'D') {
+				
+				/// Rotation
+				float Rx = BitConverter.ToSingle (RecvBuffer, sizeof(char));
+				float Ry = BitConverter.ToSingle (RecvBuffer, sizeof(char)+1 * sizeof(float));
+				float Rz = BitConverter.ToSingle (RecvBuffer, sizeof(char)+2 * sizeof(float));
+				/// Position
+				float Tx = BitConverter.ToSingle (RecvBuffer, sizeof(char)+3 * sizeof(float));
+				float Ty = BitConverter.ToSingle (RecvBuffer, sizeof(char)+4 * sizeof(float));
+				float Tz = BitConverter.ToSingle (RecvBuffer, sizeof(char)+5 * sizeof(float));
+
+				Cam.transform.rotation = Quaternion.Euler (Rx, Ry, Rz);
+				Cam.transform.position = new Vector3 (Tx, Ty, Tz);
+				print(Tx);
+			}
+		}
+	}
+
+	#endregion
+
+	#region RecvMessageFromServer
+	public  void RecvMessageFromServer(){
+
+		client.Receive (RecvBuffer);
+
+	}
+	#endregion
+
+
+
+}
---
+++ Server/Assets/Server.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.Collections;
+using UnityEngine;
+using System.Threading;
+
+public class Server : MonoBehaviour {
+
+	private byte[] SendBuffer = new byte[1024];
+	private byte[] RecvBuffer = new byte[100];
+	private char InputType;
+	public GameObject Myself;
+	//private Thread SendThread;
+	private Thread Listening;
+	Socket tcpSocket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+	Socket mSocket;
+
+	Vector3 temp;
+	// Use this for initialization
+	void Start () {
+
+		/*绑定监听消息IP和端口号*/
+		IPAddress ip = IPAddress.Parse("192.168.1.4");
+		EndPoint endPoint = new IPEndPoint(ip, 6000);
+		tcpSocket.Bind(endPoint);//向操作系统申请一个ip和端口号
+
+		Listening = new Thread (ListenClient);
+		Listening.IsBackground = true;
+		Listening.Start ();
+
+//		SendThread = new Thread (SendMessageToClient);
+//		SendThread.IsBackground = true;
+//		SendThread.Start ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			Myself.transform.position += new Vector3(0.5f,0.0f,0.0f);
+
+		}if (Input.GetKey (KeyCode.RightArrow)) {
+			Myself.transform.position -= new Vector3(0.5f,0.0f,0.0f);
+		}
+		if(mSocket != null)
+		SendMessageToClient ();
+	}
+	
+	#region SendMessageToClient
+	public void SendMessageToClient(){
+			InputType = 'D';
+			byte[] inputType_Byte = BitConverter.GetBytes ((char)InputType);
+		///Rotation
+			temp = Myself.transform.eulerAngles;
+			byte[] x_Byte = BitConverter.GetBytes ((float)temp.x);
+			byte[] y_Byte = BitConverter.GetBytes ((float)temp.y);
+			byte[] z_Byte = BitConverter.GetBytes ((float)temp.z);
+
+
+		/// Position
+			temp = Myself.transform.position;
+			byte[] Px_Byte = BitConverter.GetBytes ((float)temp.x);
+			byte[] Py_Byte = BitConverter.GetBytes ((float)temp.y);
+			byte[] Pz_Byte = BitConverter.GetBytes ((float)temp.z);
+		
+
+			inputType_Byte.CopyTo (SendBuffer, 0);
+			x_Byte.CopyTo (SendBuffer, inputType_Byte.Length);
+			y_Byte.CopyTo (SendBuffer, inputType_Byte.Length+x_Byte.Length);
+			z_Byte.CopyTo (SendBuffer, inputType_Byte.Length+x_Byte.Length + y_Byte.Length);
+
+		    Px_Byte.CopyTo(SendBuffer, inputType_Byte.Length+x_Byte.Length + y_Byte.Length + z_Byte.Length);//position x
+			Py_Byte.CopyTo(SendBuffer, inputType_Byte.Length+x_Byte.Length + y_Byte.Length + z_Byte.Length + Px_Byte.Length);//position y
+		    Pz_Byte.CopyTo(SendBuffer, inputType_Byte.Length+x_Byte.Length + y_Byte.Length + z_Byte.Length + Px_Byte.Length + Px_Byte.Length);//position z
+			
+		    mSocket.Send (SendBuffer);
+
+
+	}
+	#endregion
+	
+	private void ListenClient(){
+		/*开始监听客户端的连接请求*/
+		tcpSocket.Listen(100);//最多可以接收100个客户端请求
+		mSocket = tcpSocket.Accept();//暂停当前线程，知道接收到客户端发来的连接请求；当接收到客户端的连接请求后，在本地服务器创建一个socket与客户端连接，并返回出来
+
+	}
+	
+}
+
